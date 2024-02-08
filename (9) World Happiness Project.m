5c4ad0afd2bb9057d64070aec4e826fe12f13af1
Taina Fernandes <157399867+taifc27@users.noreply.github.com>
2024-02-07 20:29:19 -0500

    Update (9) World Happiness Project.m

--- (9) World Happiness Project.m
+++ (9) World Happiness Project.m
@@ -1,8 +1,8 @@
 ---
 name: World Happiness
 tools: [Data Analysis, MySQL]
-image: https://i.postimg.cc/g0FpFGXJ/World-Happiness.jpg
+image: [https://i.postimg.cc/g0FpFGXJ/World-Happiness.jpg]
 description: Created a dashboard on tableau using the world happiness dataset.
-external_url: https://public.tableau.com/app/profile/taina.fernandes/viz/WorldHappiness_17073529857110/Painel1?publish=yes
+external_url: [https://public.tableau.com/app/profile/taina.fernandes/viz/WorldHappiness_17073529857110/Painel1?publish=yes]
 ---
 
